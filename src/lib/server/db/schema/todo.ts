cab6e0fdf815d43ef26b060232e6337a2528be34
AdamAugustinsky <AdamAugustinsky@gmail.com>
2025-08-02 08:23:35 -0300

    drizzle pg and elysia

---
+++ src/lib/server/db/schema/todo.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, boolean, serial } from 'drizzle-orm/pg-core';
+
+export const todo = pgTable('todo', {
+	id: serial('id').primaryKey(),
+	text: text('text').notNull(),
+	completed: boolean('completed').default(false).notNull(),
+	label: text('label').notNull().$type<'bug' | 'feature' | 'documentation'>(),
+	status: text('status')
+		.notNull()
+		.$type<'backlog' | 'todo' | 'in progress' | 'done' | 'canceled'>(),
+	priority: text('priority').notNull().$type<'low' | 'medium' | 'high'>()
+});
